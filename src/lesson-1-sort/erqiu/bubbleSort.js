c211b5956c7cc02f391c4c2dd67f2e64019e0f3a
Bob Gong <gongyuan94@gmail.com>
2018-09-22 02:24:17 +0900

    bubble sort: version 0

--- src/lesson-1-sort/erqiu/bubbleSort.js
+++ src/lesson-1-sort/erqiu/bubbleSort.js
@@ -6,6 +6,36 @@ async function bubbleSort(length, isLessThan, swap) {
 
     // for swapping items
     // await swap(i, j)
+
+    // for loop
+    // for (let i = 0; i < 3; i = i + 1) {
+    //     await isLessThan(i, i + 1)
+    // }
+
+    // if statement
+    // const isNoGreater = await isLessThan(0, 1)
+    // if (isNoGreater) {
+    //     await swap(1, 2)
+    // }
+
+    for (let i = 0; i < length ; i = i + 1) {
+        for (let i = 0; i < length - 1 ; i = i + 1) {
+            if (await isLessThan(i + 1, i)) {
+                await swap(i, i + 1)
+            }
+        }
+    }
+
+
+    // const compareResult = await isLessThan(1, 3)
+    // console.log(compareResult)
+
+    // await swap(1, 3)
+
+    // const compareResult2 = await isLessThan(3, 4)
+    // console.log(compareResult2)
+
+    // await swap (3,4)
 }
 
 export default bubbleSort;
